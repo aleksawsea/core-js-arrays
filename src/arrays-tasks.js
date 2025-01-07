12a40c0f562c58da2c117675dbc0a71e359a5990
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-08 00:27:23 +0400

    feat: implement doubleArray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -230,8 +230,9 @@ function getTail(arr, n) {
  *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
  *    doubleArray([]) => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  const arr2 = [...arr];
+  return arr.concat(arr2);
 }
 
 /**
