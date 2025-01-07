5d84dc680319e41f3e38e0d9cae1ea0b93acd449
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-07 20:16:19 +0400

    feat: implement getAverage function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -132,8 +132,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (!arr.length) {
+    return 0;
+  }
+  const sum = arr.reduce((accumulator, value) => accumulator + value);
+  const avg = sum / arr.length;
+  return Number.isInteger(avg) ? avg : Number(avg.toFixed(2));
 }
 
 /**
