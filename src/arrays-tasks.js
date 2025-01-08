3b18d7387eaa57545b3e08846943bb9afe873a46
aleksawsea <alexandrapynzar57@gmail.com>
2025-01-09 01:01:02 +0400

    feat: implement getIndicesOfOddNumbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -449,8 +449,10 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const newArr = numbers.filter((n) => n % 2 === 1);
+  const finalArr = newArr.map((x) => numbers.indexOf(x));
+  return finalArr;
 }
 
 /**
